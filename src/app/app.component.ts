a63b9dfc2d21479686fcacbac77dba4b752dbe97
Craig Rouse <craig.rouse@tealium.com>
2023-08-18 09:48:15 +0100

    demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Collectors, Dispatchers, LogLevel, Tealium, TealiumConfig, TealiumEnvironment, TealiumEvent, TealiumView } from '@awesome-cordova-plugins/tealium';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,35 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(platform: Platform) {
+    platform.ready().then(() => {
+      console.log("hello");
+      let tealConfig: TealiumConfig = {
+        account: "tealiummobile",
+        profile: "demo",
+        environment: TealiumEnvironment.dev,
+        collectors: [Collectors.AppData, Collectors.Connectivity],
+        dispatchers: [Dispatchers.Collect],
+        loglevel: LogLevel.dev,
+        useRemoteLibrarySettings: false
+      };
+
+      Tealium.initialize(tealConfig).then(() => {
+        let tealView = new TealiumView("hello", new Map<string, any>([
+          ["key1", "value1"],
+          ["key2", 2]
+        ]));
+        Tealium.track(tealView);
+        let tealEvent = new TealiumEvent("cart_add", new Map<string, any>([
+          ["key1", "value1"],
+          ["key2", 2],
+          ["key3", true]
+        ]));
+        Tealium.track(tealEvent);
+      });
+    }
+
+    );
+  }
+
 }
